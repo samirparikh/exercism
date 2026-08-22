fb046bdf5314077079480e36f66713f2e8d59485
Samir Parikh <siparikh@gmail.com>
2026-08-22 09:09:45 -0400

    solve isogram

--- c/isogram/isogram.c
+++ c/isogram/isogram.c
@@ -1,1 +1,18 @@
 #include "isogram.h"
+#include <ctype.h>
+
+bool is_isogram(const char phrase[]) {
+
+    if (!phrase) return false;
+
+    char letter[26] = { false };
+    for (int i = 0; phrase[i] != '\0'; i++) {
+        char c = phrase[i];
+        if (isalpha(c)) {
+            int j = tolower(c) - 'a';
+            if (letter[j]) return false;
+            letter[j] = true;
+        }
+    }
+    return true;
+}
