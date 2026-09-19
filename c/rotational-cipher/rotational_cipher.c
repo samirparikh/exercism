ee2f30bbdc137d2804854f95f61002eee0e7c791
Samir Parikh <siparikh@gmail.com>
2026-09-19 12:35:28 -0400

    solve ciperh

--- c/rotational-cipher/rotational_cipher.c
+++ c/rotational-cipher/rotational_cipher.c
@@ -1,11 +1,27 @@
 #include "rotational_cipher.h"
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 char *rotate(const char *text, int shift_key) {
+
+    if (text == NULL) return NULL;
+
+    size_t length = strlen(text);
+    char *result = malloc(length + 1); // +1 for null terminator
+    if (result == NULL) {
+        fprintf(stderr, "malloc error\n");
+        return NULL;
+    }
+
     while (*text != '\0') {
         printf("%c\n", *text);
         text++;
     }
 
-    return *text;
+    for (size_t i = 0; i < length; i++) {
+        result[i] = text[i] + 1;
+    }
+
+    return result;
 }
